efc5f454e8b1e56ba99992ceeae065776982f988
s-a-a-m <siakoaam@gmail.com>
2025-08-07 03:06:19 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,28 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { resolve } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/spinx-clone-vision/',  // ✅ REQUIRED for GitHub Pages!
+  base: "/spinx-clone-vision/", // ✅ Required for GitHub Pages!
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
     },
   },
 }));
