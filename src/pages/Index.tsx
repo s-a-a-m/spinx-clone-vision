41769d098c7fff58a08326ea04b24edf4e529fac
s-a-a-m <siakoaam@gmail.com>
2025-08-07 04:06:48 +0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,5 @@
 import { Header } from "../components/Header";
-import { HeroSection } from ../components/HeroSection";
+import { HeroSection } from "../components/HeroSection";
 import { FeaturedProjects } from "../components/FeaturedProjects";
 import { SystemsSection } from "../components/SystemsSection";
 import { AboutSection } from "../components/AboutSection";
